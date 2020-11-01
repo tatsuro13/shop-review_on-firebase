7cffb99ce33e5b1f6a7deaf26740e101fc757436
tatsuro <oscarwild13@gmail.com>
2020-11-01 13:12:14 +0900

    レビューのフォーム制作

---
+++ src/components/TextArea.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { TextInput } from 'react-native-gesture-handler'
+
+type Props = {
+    onChangeText: (text: string) => void;
+    value: string;
+    label: string;
+    height?: number;
+    plaveHolder?: string; 
+}
+
+const TextArea: React.FC<Props> = ({value, onChangeText, label, height, plaveHolder}: Props) => {
+    return (
+        <View style={[styles.container, !!height && {height}]}>
+            <Text style={styles.label}>{label}</Text>
+            <TextInput
+            style={styles.input}
+            onChangeText={(text) => onChangeText(text)}
+            value={value}
+            multiline={true}
+            placeholder={plaveHolder}
+             />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+      padding: 16,
+    },
+    input: {
+      height: 120,
+      borderColor: "#999",
+      borderBottomWidth: 1,
+      marginTop: 8,
+    },
+    label: {
+      fontWeight: "bold",
+      color: "#999",
+    },
+  });
+
+export default TextArea
--- src/screens/CreateReviewScreen.tsx
+++ src/screens/CreateReviewScreen.tsx
@@ -1,9 +1,12 @@
 import { RouteProp } from '@react-navigation/native'
 import { StackNavigationProp } from '@react-navigation/stack'
-import React, { useEffect } from 'react'
-import { SafeAreaView, StyleSheet, Text } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { ListViewBase, SafeAreaView, StyleSheet, Text } from 'react-native'
 import { RootStackParamList } from '../types/navigation'
 import { IconButton } from '../components/IconButton'
+import TextArea from '../components/TextArea'
+import StarInput from '../components/StarInput'
+import { Value } from 'react-native-reanimated'
 
 type Props = {
     navigation: StackNavigationProp<RootStackParamList, 'CreateReview'>
@@ -12,6 +15,8 @@ type Props = {
 
 export const CreateReviewScreen:React.FC<Props> = ({navigation, route}: Props) => {
     const {shop} = route.params;
+    const [text, setText] = useState<string>('');
+    const [score, setScore] = useState<number>(3);
 
     useEffect(() => {
         navigation.setOptions({
@@ -24,7 +29,8 @@ export const CreateReviewScreen:React.FC<Props> = ({navigation, route}: Props) =
 
     return (
         <SafeAreaView>
-            <Text>Create Review Screen</Text>
+            <StarInput score={score} onChangeScore={(value) => setScore(value)} />
+            <TextArea value={text} onChangeText={(value) => setText(value)} label='レビュー' plaveHolder='レビューを書いてください' />
         </SafeAreaView>
     )
 }
